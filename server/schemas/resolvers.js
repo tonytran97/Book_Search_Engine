3989c6b02fe388bfaf9043b9393a7d594ad00617
tonytran97 <tonytran_97@live.com>
2022-12-05 12:53:01 -0800

    apollo server running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,14 +6,6 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async () => {
-            return User.find();
-        },
-
-        user: async (parent, { _id }) => {
-            return User.findOne({ _id: profileId });
-        },
-
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({ _id: context.user._id });
