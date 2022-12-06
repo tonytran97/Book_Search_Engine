955b9c3f38b92e0f15347b971fe8e35c1ba21893
tonytran97 <tonytran_97@live.com>
2022-12-05 17:16:30 -0800

    debugged errors from client pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -73,12 +73,12 @@ const SearchBooks = () => {
       // const response = await saveBook(bookToSave, token);
 
       const { data } = await saveBook({
-        variables: { args },
+        variables: { bookData: bookToSave },
       });
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
+      if (!data) {
+        throw new Error('something went wrong!');
+      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
