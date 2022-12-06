9a4a55c77285fdf11dbbd80a3858d6152f3cca7e
tonytran97 <tonytran_97@live.com>
2022-12-05 16:51:00 -0800

    refactor SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,8 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [createUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,8 +32,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
+      // const response = await createUser(userFormData);
+      const newUser = await createUser({
+        variables: { ...userFormData }
+      })
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
